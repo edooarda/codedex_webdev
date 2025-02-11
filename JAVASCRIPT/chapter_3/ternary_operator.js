affb63235549c3a518d64d0628c8719383d3e410
edooarda <edooarda@gmail.com>
2025-02-11 22:23:23 +0100

    add ternary in chapter 3

---
+++ JAVASCRIPT/chapter_3/ternary_operator.js
@@ -0,0 +1,12 @@
+let num = Math.random();
+
+// If and else Statements 
+// if (num > 0.5) {
+//   console.log("Heads");
+// } else {
+//   console.log("Tails");
+// }
+
+
+// Ternary Operator 
+num > 0.5 ? console.log("Heads") : console.log("Tails");
